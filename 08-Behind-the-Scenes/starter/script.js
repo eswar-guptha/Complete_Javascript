72b46231078903c4f6469ce60a6e0a02ba47920f
eswar <m.venkatapraveen@mvenkatapraveens-MacBook-Air.local>
2023-08-06 09:33:07 +0530

    Concept :- Primitives vs. Objects in Practice

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -76,4 +76,54 @@ console.log(jaya);
 jaya.calcAge();
 
 let fun = eswar.calcAge;
-console.log(fun);
+// console.log(fun);
+
+// /////////////////////////////////////////////
+// ===>>> Concept :- Primitives vs. Objects in Practice
+// /////////////////////////////////////////////
+
+/*
+// => Primitive types :-
+let lastName = 'Guptha';
+let oldLastName = lastName;
+lastName = 'Golla';
+
+console.log(lastName);
+console.log(oldLastName);
+
+// => Reference Types :-
+let arani = {
+  firstName: 'arani',
+  lastName: 'naga',
+  age: 27,
+};
+
+const marriedArani = arani;
+marriedArani.lastName = 'Golla';
+
+console.log('Before : ', arani);
+console.log('After : ', marriedArani);
+*/
+
+// => Copying object :-
+
+let person1 = {
+  firstName: 'Eswar',
+  lastName: 'Golla',
+  birthYear: 2000,
+  address: {
+    hNo: '1-45',
+    street: 'Kotapadu Road',
+    Village: 'pothunuru',
+  },
+};
+
+// console.log(person1);
+
+let person2 = Object.assign({}, person1);
+person2.firstName = 'Arani';
+(person2.birthYear = 1996), (person2.address.Village = 'Hyd');
+console.log(person2);
+console.log(person1);
+
+
